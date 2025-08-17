99c3bc64de84c138d2e0e9c768091095226cbd5b
Hassanhamidi6 <hassanhamidi612@gmail.com>
2025-08-17 15:02:20 +0500

    EDA for Students data

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,8 @@
 import sys
 import logging
+# from src.logger import logging 
 from datetime import datetime
+from logger import logging 
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() # exc_tb gives Information that in which file and in which line exception has occurd 
@@ -20,11 +22,11 @@ class CustomException(Exception):
 
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("Divided by zero")
-#         raise CustomException(e, sys)
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divided by zero")
+        raise CustomException(e, sys)
 
