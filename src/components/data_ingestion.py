aa45071d2d028ce7f6fd8521a82d62f8e271314e
Hassanhamidi6 <hassanhamidi612@gmail.com>
2025-08-25 16:31:37 +0500

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from src.logger import logging
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation, DataTransformationConifg
 
 
 @dataclass
@@ -44,4 +45,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj= DataIngestion()
-    obj.initiate_data_ingestion()   # After executing this artifacts folder will created also logs will updated 
+    train_data, test_data= obj.initiate_data_ingestion()   # After executing this artifacts folder will created also logs will updated 
+
+    data_transformation= DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
