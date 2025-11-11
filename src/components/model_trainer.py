a6d5025b4e2548023c620ffd948df97ac467e880
Hassanhamidi6 <hassanhamidi612@gmail.com>
2025-11-11 13:34:47 +0500

    update changes

---
+++ src/components/model_trainer.py
@@ -0,0 +1,14 @@
+from catboost import CatBoostRegressor
+from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score    
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
+from dataclasses import dataclass
+import os 
+import sys
